69b191f82b0ca246d85669c10fb1bd90ec38c31d
KGESH <seonghyeon.jee@gmail.com>
2022-08-28 15:42:46 +0900

    Refactor: change return type

--- libs/entities/src/domain/user/user.repository.ts
+++ libs/entities/src/domain/user/user.repository.ts
@@ -7,24 +7,26 @@ import { User } from './user.entity';
 export class UserRepository {
   constructor(private readonly dataSource: DataSource) {}
 
-  async createUser(createUserDto: CreateUserDto) {
+  async createUser(createUserDto: CreateUserDto): Promise<boolean> {
     const queryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
 
     try {
       const user = this.dataSource.getRepository(User).create(createUserDto);
-      const saveResult = await this.dataSource.getRepository(User).save(user);
+      await this.dataSource.getRepository(User).save(user);
 
       await queryRunner.commitTransaction();
 
-      return saveResult;
+      return true;
     } catch (e) {
       if (e.code === '23505') {
         /** Todo: Handle exception */
         console.log('Dup FK!');
       }
       await queryRunner.rollbackTransaction();
+
+      return false;
     } finally {
       await queryRunner.release();
     }
