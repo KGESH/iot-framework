a1e1c53b94955d8b2b1bb517949b0c714fac2593
KGESH <seonghyeon.jee@gmail.com>
2022-08-29 19:02:22 +0900

    Chore: styling

--- libs/entities/src/domain/device/slave/index.ts
+++ libs/entities/src/domain/device/slave/index.ts
@@ -1,2 +1,4 @@
 export * from './slave.module';
 export * from './slave.entity';
+export * from './slave-query.repository';
+export * from './slave-config';
--- libs/entities/src/index.ts
+++ libs/entities/src/index.ts
@@ -4,6 +4,8 @@ export * from './domain/device/slave';
 export * from './domain/device/led';
 export * from './domain/device/water-pump';
 export * from './domain/device/temperature';
+export * from './domain/device/protocol/iot-gateway-packet';
+export * from './domain/device/protocol/types/protocol.enum';
 
 /** User Microservice */
 export * from './domain/user';
