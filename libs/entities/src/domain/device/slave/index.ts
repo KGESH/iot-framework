0cb3fd4201b0c1e5cacb3fc8b43491c4056b831b
KGESH <seonghyeon.jee@gmail.com>
2022-08-31 18:56:07 +0900

    style: change libs path

--- libs/entities/src/domain/device/slave/index.ts
+++ libs/entities/src/domain/device/slave/index.ts
@@ -2,3 +2,9 @@ export * from './slave.module';
 export * from './slave.entity';
 export * from './slave-query.repository';
 export * from './slave-config';
+
+export * from './sensors/led';
+export * from './sensors/water-pump';
+export * from './sensors/temperature';
+export * from './sensors/dto/sensor-power.dto';
+export * from './sensors/dto/sensors-config.dto';
--- libs/entities/src/index.ts
+++ libs/entities/src/index.ts
@@ -1,9 +1,6 @@
 /** Device Microservice */
 export * from './domain/device/master';
 export * from './domain/device/slave';
-export * from './domain/device/led';
-export * from './domain/device/water-pump';
-export * from './domain/device/temperature';
 export * from './domain/device/protocol/iot-gateway-packet';
 export * from './domain/device/protocol/types/protocol.enum';
 
