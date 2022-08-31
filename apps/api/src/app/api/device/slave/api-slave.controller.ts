992c0bfd9b58f406fc70a266c310bed59a9671cd
KGESH <seonghyeon.jee@gmail.com>
2022-08-31 18:59:17 +0900

    style: cleanup & add return type

--- apps/api/src/app/api/device/slave/api-slave.controller.ts
+++ apps/api/src/app/api/device/slave/api-slave.controller.ts
@@ -1,62 +1,43 @@
+import { Body, Controller, Get, Delete, Headers, Post, Query, UseGuards } from '@nestjs/common';
 import {
-  Body,
-  Controller,
-  Get,
-  Delete,
-  Headers,
-  Post,
-  Query,
-  UseGuards,
-} from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ApiMasterService } from '../master/api-master.service';
 import { ApiSlaveService } from './api-slave.service';
 import { SWAGGER_TAG } from '../../../../utils/swagger/enum';
-import { JwtAuthGuard, RolesGuard } from '@iot-framework/modules';
-import { UserRoles } from '@iot-framework/entities';
+import { JwtAuthGuard, ResponseEntity, RolesGuard } from '@iot-framework/modules';
+import { Slave, SlaveConfigsResponse, UserRoles } from '@iot-framework/entities';
 import { CreateSlaveDto } from './dto/create-slave.dto';
 
 @ApiTags(SWAGGER_TAG.SLAVE)
 @ApiBearerAuth()
 @Controller('device/slave')
 export class ApiSlaveController {
-  constructor(
-    private masterService: ApiMasterService,
-    private slaveService: ApiSlaveService
-  ) {}
+  constructor(private masterService: ApiMasterService, private slaveService: ApiSlaveService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: ResponseEntity })
   @UseGuards(RolesGuard([UserRoles.ADMIN, UserRoles.USER]))
   @UseGuards(JwtAuthGuard)
-  async createSlave(@Body() createSlaveDto: CreateSlaveDto) {
-    try {
-      console.log(`Call Slave Create`);
-      const data = await this.slaveService.createSlave(createSlaveDto);
-      console.log(`Get result: `, data);
-
-      return data;
-    } catch (e) {
-      console.log(e);
-    }
+  async createSlave(@Body() createSlaveDto: CreateSlaveDto): Promise<ResponseEntity<Slave>> {
+    return this.slaveService.createSlave(createSlaveDto);
   }
 
   @Delete()
   @UseGuards(RolesGuard([UserRoles.ADMIN]))
   @UseGuards(JwtAuthGuard)
-  async deleteSlave(
-    @Query('master_id') masterId: number,
-    @Query('slave_id') slaveId: number
-  ) {
+  async deleteSlave(@Query('master_id') masterId: number, @Query('slave_id') slaveId: number) {
     return this.slaveService.deleteSlave(masterId, slaveId);
   }
 
   @Get('state')
   @UseGuards(RolesGuard([UserRoles.ADMIN, UserRoles.USER]))
   @UseGuards(JwtAuthGuard)
-  async getSensorsState(
-    @Query('master_id') masterId: number,
-    @Query('slave_id') slaveId: number
-  ) {
+  async getSensorsState(@Query('master_id') masterId: number, @Query('slave_id') slaveId: number) {
     return this.slaveService.getSlaveState(masterId, slaveId);
   }
 
@@ -64,10 +45,9 @@ export class ApiSlaveController {
   @UseGuards(RolesGuard([UserRoles.ADMIN, UserRoles.USER]))
   @UseGuards(JwtAuthGuard)
   async fetchConfig(
-    @Headers() header: any,
     @Query('master_id') masterId: number,
     @Query('slave_id') slaveId: number
-  ) {
+  ): Promise<ResponseEntity<SlaveConfigsResponse>> {
     return this.slaveService.getSlaveConfigs(masterId, slaveId);
   }
 }
