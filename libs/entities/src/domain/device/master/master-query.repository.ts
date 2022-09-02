4289de82d7ef79c3164bba9e514ee0b6ea6197bc
KGESH <seonghyeon.jee@gmail.com>
2022-09-02 22:28:22 +0900

    Feat: add master query or fail method

--- libs/entities/src/domain/device/master/master-query.repository.ts
+++ libs/entities/src/domain/device/master/master-query.repository.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { Master } from '@iot-framework/entities';
+import { ResponseEntity } from '@iot-framework/modules';
 
 @Injectable()
 export class MasterQueryRepository {
@@ -8,6 +9,15 @@ export class MasterQueryRepository {
 
   async findOneByMasterId(masterId: number): Promise<Master> {
     return this.dataSource.getRepository(Master).findOneBy({ masterId });
+  }
+
+  async findOneByMasterIdOrFail(masterId: number): Promise<Master> {
+    const master = await this.dataSource.getRepository(Master).findOneBy({ masterId });
+    if (!master) {
+      throw ResponseEntity.ERROR_WITH(`Master Not Found!`, HttpStatus.BAD_REQUEST);
+    }
+
+    return master;
   }
 
   async findMastersByUserId(userId: number): Promise<Master[]> {
