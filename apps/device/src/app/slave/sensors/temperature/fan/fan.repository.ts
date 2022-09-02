e70d463286c3cad453820cab24145010760ca397
KGESH <seonghyeon.jee@gmail.com>
2022-09-02 22:25:40 +0900

    Refactor: extract throw exception

--- apps/device/src/app/slave/sensors/temperature/fan/fan.repository.ts
+++ apps/device/src/app/slave/sensors/temperature/fan/fan.repository.ts
@@ -2,13 +2,23 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { DataSource, UpdateResult } from 'typeorm';
 import { Slave, Thermometer } from '@iot-framework/entities';
 import { EPowerState } from '@iot-framework/utils';
-import { ResponseEntity } from '@iot-framework/modules';
+import { notAffected, ResponseEntity } from '@iot-framework/modules';
 
 @Injectable()
 export class FanRepository {
   constructor(private readonly dataSource: DataSource) {}
 
-  async updateFanPowerState(slave: Slave, powerState: EPowerState): Promise<UpdateResult> {
+  async updateFanPowerState(slave: Slave, powerState: EPowerState): Promise<void> {
+    const updateResult = await this.updatePowerState(slave, powerState);
+    if (notAffected(updateResult)) {
+      throw ResponseEntity.ERROR_WITH(
+        'Fan power state update not affected!',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  async updatePowerState(slave: Slave, powerState: EPowerState): Promise<UpdateResult> {
     const queryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
