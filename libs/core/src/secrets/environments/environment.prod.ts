e6d5380c10f1c1bdc8bda1983090b8ad7e11db13
KGESH <seonghyeon.jee@gmail.com>
2022-09-11 23:49:28 +0900

    Chore: change test aws db endpoint

--- libs/core/src/secrets/environments/environment.prod.ts
+++ libs/core/src/secrets/environments/environment.prod.ts
@@ -25,13 +25,13 @@ export const environment = {
   USER_MS_URL: 'auth/user/',
 
   DATABASE_TYPE: 'postgres',
-  DATABASE_HOST: 'iot-cluster-rds.cshttetfsiot.ap-northeast-2.rds.amazonaws.com',
+  DATABASE_HOST: 'iot-public-cluster.cshttetfsiot.ap-northeast-2.rds.amazonaws.com',
   DATABASE_NAME: 'master',
   DATABASE_USER: 'postgres',
   DATABASE_PASSWORD: 'gksdldma123',
   DATABASE_PORT: 5432,
 
-  REDIS_HOST: 'iot-cluster-cache.hrfbrq.ng.0001.apn2.cache.amazonaws.com',
+  REDIS_HOST: 'iot-public-cluster-cache.hrfbrq.ng.0001.apn2.cache.amazonaws.com',
   REDIS_PORT: 6379,
 
   MQTT_BROKER_URL: 'mqtt://43.200.81.115:1883',
