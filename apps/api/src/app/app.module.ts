19eab11a3279b456e477ac935d25c186f956e96e
KGESH <seonghyeon.jee@gmail.com>
2022-09-12 00:45:52 +0900

    Refactor: remove database module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,23 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from '@iot-framework/core';
 import { HealthController } from './health.controller';
-import {
-  DatabaseModule,
-  MicroserviceClientsModule,
-  RedisModule,
-} from '@iot-framework/modules';
+import { MicroserviceClientsModule, RedisModule } from '@iot-framework/modules';
 import { ApiAuthModule } from './api/auth/api-auth.module';
 import { ApiDeviceModule } from './api/device/api-device.module';
 
 @Module({
-  imports: [
-    CoreModule,
-    DatabaseModule,
-    RedisModule,
-    MicroserviceClientsModule,
-    ApiAuthModule,
-    ApiDeviceModule,
-  ],
+  imports: [CoreModule, RedisModule, MicroserviceClientsModule, ApiAuthModule, ApiDeviceModule],
   controllers: [HealthController],
 })
 export class AppModule {}
